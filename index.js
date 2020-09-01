9db20ea34d3de8fe258ea52ebbec60a0d8b435aa
Ashwyn D. Horton <ashwynh21@gmail.com>
2020-09-01 17:43:24 +0200

    Minor error on server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(`${__dirname}/dist/website`));
 app.listen(process.env.PORT || 80);
 
 console.log(`Ignitive Labs is Listening on port 80`);
