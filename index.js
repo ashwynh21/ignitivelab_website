dedbc20bb20adfc3390a1538302bf9b2be15584f
Ashwyn D. Horton <ashwynh21@gmail.com>
2020-09-01 17:34:05 +0200

    Minor error on server

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
-app.get('/', function (request, response) {
-  response.redirect('/register');
-});
-app.listen(process.env.PORT || 4300);
+app.listen(process.env.PORT || 80);
 
-console.log(`Ngena Server is Listening on port 4300`);
+console.log(`Ignitive Labs is Listening on port 80`);
